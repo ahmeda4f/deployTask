b35eff53a65688bb5ca8185b24ada41423a13d9b
Ahmed <127054826+ahmeda4f@users.noreply.github.com>
2025-10-01 14:23:47 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ def load_model(task):
     else:
         model_path = hf_hub_download(
             repo_id="Ahmed-Ashraf-00/brain_tumour_testing",
-            filename="best_model_segmentation.keras",
+            filename="content/best_model_segmentation.keras",
             token=st.secrets.get("HF_TOKEN")
         )
     return keras.models.load_model(model_path, compile=False)
@@ -49,3 +49,4 @@ if uploaded_file is not None:
         mask = model.predict(arr)[0]
         mask = (mask > 0.5).astype(np.uint8) * 255
         st.image(mask, caption="Predicted Mask", use_container_width=True)
+
